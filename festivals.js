b9e4306aa92d1e1f784a71e07abba93675323f97
姚威 <yw>
2026-06-08 08:44:32 +0800

    first commit

---
+++ festivals.js
@@ -0,0 +1,24 @@
+/** 节日数据，小程序从此文件加载 */
+module.exports = [
+  { year: 2026, name: '元旦', start: '1.1', end: '1.3' },
+  { year: 2026, name: '春节', start: '2.15', end: '2.23' },
+  { year: 2026, name: '第2学期开学', start: '3.2', end: '-', hideOnHome: true },
+  { year: 2026, name: '清明节', start: '4.4', end: '4.6' },
+  { year: 2026, name: '劳动节', start: '5.1', end: '5.5' },
+  { year: 2026, name: '端午节', start: '6.19', end: '6.21' },
+  { year: 2026, name: '第2学期暑假', start: '7.8', end: '8.31', hideOnHome: true },
+  { year: 2026, name: '第1学期开学', start: '9.1', end: '-', hideOnHome: true },
+  { year: 2026, name: '中秋节', start: '9.25', end: '9.27' },
+  { year: 2026, name: '国庆节', start: '10.1', end: '10.7' },
+  { year: 2027, name: '元旦', start: '1.1', end: '1.3' },
+  { year: 2027, name: '第1学期寒假', start: '1.18', end: '2.21', hideOnHome: true },
+  { year: 2027, name: '春节', start: '2.5', end: '2.13' },
+  { year: 2027, name: '第2学期开学', start: '2.22', end: '-', hideOnHome: true },
+  { year: 2027, name: '清明节', start: '4.4', end: '4.6' },
+  { year: 2027, name: '劳动节', start: '5.1', end: '5.5' },
+  { year: 2027, name: '端午节', start: '6.15', end: '6.17' },
+  { year: 2027, name: '第2学期暑假', start: '7.10', end: '8.31', hideOnHome: true },
+  { year: 2027, name: '第1学期开学', start: '9.1', end: '-', hideOnHome: true },
+  { year: 2027, name: '中秋节', start: '9.22', end: '9.24' },
+  { year: 2027, name: '国庆节', start: '10.1', end: '10.7' }
+]
